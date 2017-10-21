df0703a6e85728bf08f5739528736a6424ed6278
GabrielDS <gabriel.santosti01@gmail.com>
2017-10-21 17:59:11 -0300

    Define controller

--- src/controller/ScreenClockController.java
+++ src/controller/ScreenClockController.java
@@ -1,4 +1,53 @@
-public class ScreenClockController
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import java.util.Calendar;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+
+public class ScreenClockController implements Initializable
 {
 
+	@FXML
+	Text time;
+
+	@FXML
+	Text period;
+
+	Calendar time_system;
+
+	SimpleDateFormat sdf_time;
+
+	SimpleDateFormat sdf_period;
+
+	public ScreenClockController()
+	{
+		sdf_time = new SimpleDateFormat("KK:mm");
+
+		sdf_period = new SimpleDateFormat("a");
+
+		time_system = new GregorianCalendar();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb)
+	{
+	    time.setText( formatTextTime() );
+
+	    period.setText( formatTextPeriod() );
+	}
+
+	public String formatTextTime()
+	{
+		return sdf_time.format( time_system.getTime() );
+	}
+
+	public String formatTextPeriod()
+	{
+		return sdf_period.format( time_system.getTime() );
+	}
 }
