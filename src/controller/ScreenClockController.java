e99ae3dd4ddbfc112d6499deb7e9a506d43b9020
GabrielDS <gabriel.santosti01@gmail.com>
2017-11-11 17:54:06 -0300

    Add thread to the clock

--- src/controller/ScreenClockController.java
+++ src/controller/ScreenClockController.java
@@ -6,23 +6,37 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import java.util.GregorianCalendar;
 import java.util.Date;
+import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.text.SimpleDateFormat;
 
-public class ScreenClockController implements Initializable
+public class ScreenClockController implements Initializable, Runnable
 {
 
 	@FXML
-	Text time;
+	private Text time;
 
 	@FXML
-	Text period;
+	private Text period;
 
-	Calendar time_system;
+	@FXML
+	ImageView img;
 
-	SimpleDateFormat sdf_time;
+	private Calendar time_system;
 
-	SimpleDateFormat sdf_period;
+	private SimpleDateFormat sdf_time;
+
+	private SimpleDateFormat sdf_period;
+
+	private Thread interna;
+
+	private String[] img_background;
+
+	private boolean stop;
+
+	private Random random_img;
 
 	public ScreenClockController()
 	{
@@ -30,19 +44,67 @@ public class ScreenClockController implements Initializable
 
 		sdf_period = new SimpleDateFormat("a");
 
-		time_system = new GregorianCalendar();
+		interna = null;
+
+		stop = false;
+
+		interna = new Thread( this, "Relogio" );
+
+		img_background = new String[ 7 ];
+
+		img_background[ 0 ] = "0.jpg";
+		img_background[ 1 ] = "1.jpg";
+		img_background[ 2 ] = "2.jpg";
+		img_background[ 3 ] = "3.jpg";
+		img_background[ 4 ] = "4.jpg";
+		img_background[ 5 ] = "5.jpg";
+		img_background[ 6 ] = "6.jpg";
+
+		random_img = new Random();
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb)
 	{
-	    time.setText( formatTextTime() );
+		img.setImage( new Image ( img_background[ random_img.nextInt( 6 ) ] ) );
 
-	    period.setText( formatTextPeriod() );
+		time.setText( formatTextTime() );
+
+		period.setText( formatTextPeriod() );
+
+		interna.start();
+	}
+
+	@Override
+	public void run()
+	{
+		while( Thread.currentThread() == interna && !stop )
+		{
+			try
+			{
+				Thread.sleep( 1000 );
+
+				time.setText( formatTextTime() );
+
+				period.setText( formatTextPeriod() );
+			}
+			catch( InterruptedException e )
+			{
+				// TODO
+			}
+		}
+	}
+
+	public void stop()
+	{
+		stop = true;
+		interna = null;
 	}
 
 	public String formatTextTime()
 	{
+		time_system = new GregorianCalendar();
+
 		return sdf_time.format( time_system.getTime() );
 	}
 
